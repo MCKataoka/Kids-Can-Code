546114ed2c91e628d054613c51ea6f6201ee6198
Mario C Kataoka <mario.c.kataoka@gmail.com>
2017-11-13 09:27:33 -0500

    Creating Settings
    This is where the most basic set up will go like screen dimensions and colors

---
+++ settings.py
@@ -0,0 +1,12 @@
+# game options/setting
+title = "My Game"
+width = 480
+height = 600
+fps = 60
+
+# define colors
+White = (255,255,255)
+Red = (255,0,0)
+Black = (0,0,0)
+Green = (0,255,0)
+Blue = (0,0,255)
