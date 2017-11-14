686ecb343de3fb5c98ef0f2bc32bc88b2e2ed776
Mario C Kataoka <mario.c.kataoka@gmail.com>
2017-11-14 17:53:18 -0500

    Update of Main based on Video 19

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # Jumpy! - platform game.
-# NEW STUFF 7
+
 import pygame as pg # so now you can just write pg.quit instead of pygame.quit()
 import random
-#NEW STUFF 2 (you can just import but then you would have to do things like settings.width)
+
 from settings import *
+#NEW STUFF 1
+from sprites import *
 
 
-#NEW STUFF 5
+
 class Game:
 	def __init__(self):
-		# NEW STUFF 9 make sure its all pg
+		
 		# initialize game window, etc
 		pg.init()
 		pg.mixer.init()
@@ -20,15 +22,17 @@ class Game:
 
 	def new(self):
 		# start a new game
-		# NEW STUFF 11 make sure its all pg
+		
 		self.all_sprites = pg.sprite.Group()
-		#NEW STUFF 20
+		#NEW STUFF 4
+		self.player = Player()
+		self.all_sprites.add(self.player)
 		self.run()
-		#END OF NEW STUFF 20
+	
 
 	def run(self):
 		#Game Loop 
-		#NEW STUFF 12 
+		
 		self.playing = True
 		while self.playing:
 			self.clock.tick(fps)
@@ -38,24 +42,24 @@ class Game:
 
 	def update(self):
 		# Game Loop - Update
-		#NEW STUFF 17
+		
 		self.all_sprites.update()
 
 	def events(self):
 		# Game Loop - events
-		#NEW STUFF 14 Dont forget the pg!
+		
 		# Process input (events)
 		for event in pg.event.get():
 			# check for closing the window
 			if event.type == pg.QUIT:
-				#NEW STUFF 1516
+				
 				if self.playing:
 					self.playing = False
 				self.running = False
 
 	def draw(self):
 		# Game Loop - draw
-		#NEW STUFF 19 dont forget selfs
+		
 		self.screen.fill(Black)
 		self.all_sprites.draw(self.screen)
 		# *after* drawing everything, flip the display
@@ -69,7 +73,7 @@ class Game:
 		# game over/continue
 		pass
 
-# NEW STUFF 6
+
 g = Game()
 g.show_start_screen()
 while g.running:
