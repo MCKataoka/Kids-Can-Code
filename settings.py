5f981ab8557b6f7d1179baa8018a37abbd94f2fe
Mario C Kataoka <mario.c.kataoka@gmail.com>
2017-11-14 17:54:13 -0500

    Update to settings based on Video 19

--- settings.py
+++ settings.py
@@ -1,8 +1,13 @@
 # game options/setting
-title = "My Game"
+title = "Jumpy!"
 width = 480
 height = 600
 fps = 60
+
+#NEW STUFF 12
+#Player Properties
+Player_acc = 0.5
+player_friction = -0.12
 
 # define colors
 White = (255,255,255)
@@ -10,3 +15,5 @@ Red = (255,0,0)
 Black = (0,0,0)
 Green = (0,255,0)
 Blue = (0,0,255)
+#NEW STUFF 3
+Yellow = (255,255,0)
