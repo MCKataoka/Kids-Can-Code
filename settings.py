fbb236ba763063e56b4946069063168a989c5a3a
Mario C Kataoka <mario.c.kataoka@gmail.com>
2017-11-20 17:33:52 -0500

    First Commit Video 20

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ width = 480
 height = 600
 fps = 60
 
-#NEW STUFF 12
+
 #Player Properties
 Player_acc = 0.5
 player_friction = -0.12
@@ -15,5 +15,5 @@ Red = (255,0,0)
 Black = (0,0,0)
 Green = (0,255,0)
 Blue = (0,0,255)
-#NEW STUFF 3
+
 Yellow = (255,255,0)
